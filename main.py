b43a067ada2c26c23e301f8817cfaf26d06e2971
Chiel92 <ctje92@gmail.com>
2015-10-24 17:02:50 +0200

    Simple example of two color changing cells

--- main.py
+++ main.py
@@ -1,98 +1,63 @@
 __version__ = '1.0'
 
+# Enable importing modules from graph-problems folder
+import sys
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, current_dir + '/graph-problems')
+
+
+import pixelgraph
 
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.clock import Clock
-from kivy.graphics import Rectangle, Color
 
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
+class ScreenCell(Widget):
+    color = NumericProperty(0.0)
+    color_delta = 0
+    speed = 1
 
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
-
-
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
-
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
-
-    red = NumericProperty(0.0)
-    green = NumericProperty(0.0)
-    blue = NumericProperty(0.0)
-
-    direction = 1
-
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
+    def toggle(self):
+        if self.color == 0:
+            self.color_delta = self.speed
+        elif self.color == 1:
+            self.color_delta = -self.speed
+        else:
+            self.color_delta *= -1
 
     def update(self, dt):
-        self.ball.move()
+        self.color += self.color_delta * dt
+        if not 0 < self.color < 1:
+            self.color = round(self.color)
+            self.color_delta = 0
 
-        self.red = self.red + self.direction * 0.01
-        if not 0 < self.red < 1:
-            self.direction *= -1
 
-        #bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
+class SkatGame(Widget):
+    cell1 = ObjectProperty(None)
+    cell2 = ObjectProperty(None)
 
-        #bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
-
-        #went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.x > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
+    def update(self, dt):
+        self.cell1.update(dt)
+        self.cell2.update(dt)
 
     def on_touch_down(self, touch):
-        self.red = 1.0 - self.red
+        if self.cell1.collide_point(touch.x, touch.y):
+            self.cell1.toggle()
+        if self.cell2.collide_point(touch.x, touch.y):
+            self.cell2.toggle()
 
 
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        elif touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+class SkatApp(App):
 
-
-class PongApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
+        game = SkatGame()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 
 
-
-
-
 if __name__ == '__main__':
-    PongApp().run()
-
-
-
+    SkatApp().run()
